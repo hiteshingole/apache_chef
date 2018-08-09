9c43ee8b70df69d65b7be3c327fbab22c90f62a7
vagrant <vagrant@localhost.localdomain>
2018-08-09 06:10:32 +0000

    added Ohai functaionality

--- recipes/server.rb
+++ recipes/server.rb
@@ -5,7 +5,9 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 package 'httpd'
 file '/var/www/html/index.html' do
-	content 'Hello World !!'
+	content "<h1>Hello World !!</h1>
+<h2> Welcome to host #{node['hostname']}</h2>
+<h3> The ip address of this host is #{node['ipaddress']}</h3>"
 end
 
 service 'httpd' do
