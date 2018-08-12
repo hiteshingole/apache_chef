f7960089115b54b0cf97b1eee718d37be73ecb1e
vagrant <vagrant@localhost.localdomain>
2018-08-12 18:15:21 +0000

    added remote file

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,6 +4,12 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 package 'httpd'
+
+remote_file '/var/www/html/google.png' do
+
+source 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+end
+
 template '/var/www/html/index.html' do
 	source 'index.html.erb'
 	action :create
