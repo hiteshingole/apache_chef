0a6d64e86042f38507c7f3125bad45b226cbc807
vagrant <vagrant@localhost.localdomain>
2018-08-12 17:31:27 +0000

    used template to genrate the html file

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,10 +4,9 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 package 'httpd'
-file '/var/www/html/index.html' do
-	content "<h1>Hello World !!</h1>
-<h2> Welcome to host #{node['hostname']}</h2>
-<h3> The ip address of this host is #{node['ipaddress']}</h3>"
+template '/var/www/html/index.html' do
+	source 'index.html.erb'
+	action :create
 end
 
 service 'httpd' do
